ab83a16b9134ef05913a0b6b669e1754af2fd363
RaitisVilums <rvilums80@gmail.com>
2023-04-22 16:40:17 +0300

    making progress

--- src/Components/Restaurant/restaurant.component.jsx
+++ src/Components/Restaurant/restaurant.component.jsx
@@ -5,8 +5,7 @@ import restaurants from "../../Data/dummyData.json";
 import Gallery from "./Components/Gallery/gallery.component";
 import Info from "./Components/Info/info.component";
 import AsideMap from "./Components/Aside/aside.component";
-import Logo from "../../Assets/logo.png";
-import Rating from "../Common/Rating/rating";
+import Reviews from "./Components/Reviews/reviews.component";
 const Restaurant = () => {
   const { id } = useParams();
 
@@ -44,36 +43,7 @@ const Restaurant = () => {
         />
         <AsideMap location={location} phone={phone} webpage={webpage} />
         <Gallery />
-        <div className="restaurant__reviews">
-          <h1 className="restaurant__reviews--title">Reviews</h1>
-          <div className="restaurant__reviews--stats">
-            <Rating rating={rating} />
-          </div>
-          <div className="restaurant__reviews--container">
-            <div className="restaurant__reviews--container__item">
-              <img src={Logo} alt="" />
-              <div className="container__item--content">
-                <div className="user-info">
-                  <h3 className="user-info__user">User User</h3>
-                  <Rating rating={rating} />
-                  <p className="user-info__date">23/4/23</p>
-                </div>
-                <p className="user-info__description">{description}</p>
-              </div>
-            </div>
-            <div className="restaurant__reviews--container__item">
-              <img src={Logo} alt="" />
-              <div className="container__item--content">
-                <div className="user-info">
-                  <h3 className="user-info__user">User User</h3>
-                  <Rating rating={rating} />
-                  <p className="user-info__date">23/4/23</p>
-                </div>
-                <p className="user-info__description">{description}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Reviews rating={rating} description={description} />
       </section>
     </MainWrapper>
   );
